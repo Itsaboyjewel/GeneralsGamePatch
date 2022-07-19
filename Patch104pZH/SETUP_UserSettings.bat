e65671fc1978175ef01dc529b1461b8b50ef328b
alanblack <31726131+alanblack166@users.noreply.github.com>
2022-07-19 18:32:01 +0300

    things

---
+++ Patch104pZH/SETUP_UserSettings.bat
@@ -0,0 +1,10 @@
+:: Copy this file, remove the .txt extension from the file name, and edit it depending on your requirements.
+
+:: Set game install root directory
+set GameRootDir="F:\Program Files (x86)\EA Games\clean copy of ZH - Copy"
+
+:: Set game executable path and file relative to root directory
+set GameExeFile="launcher\generals.exe"
+
+:: Set game launch command line arguments
+set GameExeArgs="-win -quickstart -noshellmap"
